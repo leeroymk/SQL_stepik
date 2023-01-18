807d7605cdc9b530c5661037e9134f193215432b
leeroymk <nikekursk@gmail.com>
2023-01-18 15:59:31 +0300

    Learn SQL with Stepik

--- fine.sql
+++ fine.sql
@@ -33,7 +33,35 @@ VALUES ('Превышение скорости(от 20 до 40)', 500),
        ('Превышение скорости(от 40 до 60)', 1000),
        ('Проезд на запрещающий сигнал', 1000);
 
-
 UPDATE fine f, traffic_violation tv
 SET f.sum_fine = tv.sum_fine
 WHERE f.sum_fine IS NULL AND f.violation = tv.violation;
+
+
+/* Use GROUP BY for few columns */
+SELECT name, number_plate, violation FROM fine
+GROUP BY name, number_plate, violation
+HAVING count(name) > 1
+ORDER BY 1, 2, 3;
+
+UPDATE fine,
+    (SELECT name, number_plate, violation
+     FROM fine
+     GROUP BY name, number_plate, violation
+     HAVING count(*) > 1) query_in
+SET fine.sum_fine = fine.sum_fine * 2
+WHERE (fine.violation, fine.name, fine.number_plate) =
+      (query_in.violation, query_in.name, query_in.number_plate) AND
+      fine.date_payment IS NULL;
+
+UPDATE fine f, payment p
+SET f.date_payment = if(f.date_payment IS NULL, p.date_payment, f.date_payment),
+f.sum_fine = if(p.date_payment - p.date_violation <= 20, f.sum_fine / 2, f.sum_fine)
+WHERE (f.name,f.number_plate,f.violation) = (p.name,p.number_plate,p.violation);
+
+CREATE TABLE back_payment AS
+SELECT name, number_plate, violation, sum_fine, date_violation FROM fine
+WHERE fine.date_payment IS NULL;
+
+DELETE FROM fine
+WHERE date_violation < '2020-02-01';
