bd27aec8fe0d1a93317b745de6bad1dbc23b17b2
leeroymk <nikekursk@gmail.com>
2023-01-26 07:44:20 +0300

    Learn SQL queries with Stepik

--- newbook.sql
+++ newbook.sql
@@ -33,3 +33,35 @@ GROUP BY name_author
 HAVING Количество < 10 or Количество IS NULL
 ORDER BY 2
 ;
+
+
+SELECT name_author
+FROM book JOIN author ON book.author_id = author.author_id
+GROUP BY name_author
+HAVING COUNT(DISTINCT(genre_id)) = 1;
+
+
+SELECT title, name_author, name_genre, price, amount FROM
+genre JOIN book ON genre.genre_id = book.genre_id
+      JOIN author on book.author_id = author.author_id
+GROUP BY title, name_author, name_genre, price, amount, genre.genre_id
+HAVING genre.genre_id IN
+(SELECT all_genre.genre_id FROM
+(SELECT genre_id, sum(amount) sum_amount
+FROM book GROUP BY genre_id) all_genre
+JOIN (SELECT genre_id, sum(amount) sum_amount FROM book
+GROUP BY genre_id
+ORDER BY sum_amount DESC
+LIMIT 1) max_genre
+ON all_genre.sum_amount = max_genre.sum_amount)
+ORDER BY title;
+
+
+/* using USING instead of ON */
+SELECT title Название,
+name_author Автор,
+SUM(book.amount + supply.amount) Количество
+FROM supply
+JOIN book USING(title, price)
+JOIN author USING(author_id)
+GROUP BY author.name_author, book.title;
