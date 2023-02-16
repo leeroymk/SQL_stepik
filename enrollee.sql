8de2892047ce166978548a8a1234d9fd471b2e21
leeroymk <nikekursk@gmail.com>
2023-02-16 15:52:12 +0300

    Learning SQL with Stepik

--- enrollee.sql
+++ enrollee.sql
@@ -66,6 +66,15 @@ JOIN program USING(program_id)
 JOIN program_subject USING(program_id)
 JOIN subject USING(subject_id)
 JOIN enrollee_subject USING(enrollee_id, subject_id)
-GROUP BY 1, 2
-ORDER BY 1, 3 DESC
-;
+GROUP BY name_program, name_enrollee
+ORDER BY name_program, itog DESC;
+
+
+SELECT DISTINCT name_program, name_enrollee
+FROM enrollee JOIN program_enrollee USING(enrollee_id)
+JOIN program USING(program_id)
+JOIN program_subject USING(program_id)
+JOIN subject USING(subject_id)
+JOIN enrollee_subject USING(enrollee_id, subject_id)
+WHERE result < min_result
+ORDER BY name_program, name_enrollee;
